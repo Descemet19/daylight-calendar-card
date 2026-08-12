887c9097c29d6ff5f580a72e6fc43da1b4b77f6c
Descemet19 <matthew.beard3@gmail.com>
2026-08-12 09:24:41 -0400

    Changes to show two weeks on week view for large display

--- src/views/week-view-model.js
+++ src/views/week-view-model.js
@@ -25,7 +25,7 @@ export function getWeekDays({ currentDate, weekStart, weekDays, rollingDays }) {
   }
 
   const days = [];
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 14; i++) { // Change to 14 to show two weeks
     const date = new Date(weekStart);
     date.setDate(weekStart.getDate() + i);
     if (weekDays.includes(date.getDay())) {
